da7de60e20c5127ae803dfb68650fac11e8d6d5a
Pankaj Jain <pankaj_jain@epam.com>
2024-04-26 16:41:37 +0530

    task

---
+++ task01/src/lambdas/hello_world/handler.py
@@ -0,0 +1,29 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        # Implementing business logic to create the response
+        response = {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
+        print(response)
+        return 200
+
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
