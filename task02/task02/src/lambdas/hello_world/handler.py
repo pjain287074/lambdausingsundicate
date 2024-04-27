48067afc60fbfc940f0d178a88ca939000cacc3d
Pankaj Jain <pankaj_jain@epam.com>
2024-04-27 21:29:00 +0530

    functional url

---
+++ task02/task02/src/lambdas/hello_world/handler.py
@@ -0,0 +1,42 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+import json
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        # Check if the request path is /hello
+        if event.get('path') == '/hello':
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"message": "Hello from Lambda"})
+            }
+        else:
+            # Return None to indicate that the request is not for this endpoint
+            return None
+
+    def handle_request(self, event, context):
+        """
+        Explain incoming event here
+        """
+        # todo implement business logic
+        # if event.get('path') == '/hello':
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Hello from Lambda"})
+        }
+
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    # Validate the request
+    response = HANDLER.validate_request(event)
+    if response:
+        return response
+
+    # If the request is not for /hello, handle it using the handle_request method
+    return HANDLER.lambda_handler(event=event, context=context)
